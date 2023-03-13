264f93a518e6dafc69510c947a5fbbed5153c39c
ReeceRenninger <renningerreece@gmail.com>
2023-03-13 13:44:10 -0700

    got all elements rendering, except the image

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import Footer from './Footer';
 
 class Main extends React.Component {
   render(){
-   return <HornedBeast/>
-  //  <Hornedbeast title="" description="" imageurl=""/>
+   return(
+    <>
+     <HornedBeast titleOne = "First Image" descriptionOne = "First image description"/>
+     <HornedBeast titleTwo = "Second Image" descriptionTwo ="Second image description"/>
+  {/* <Hornedbeast title="" description="" imageurl=""/> */}
+  </>
+   )
   }
 }
 
