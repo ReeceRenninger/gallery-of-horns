cbfd1e0a23b2de87a3bddd7d93e15620d1b636b4
ReeceRenninger <renningerreece@gmail.com>
2023-03-14 12:51:53 -0700

    got data working, need to fix styling and buttons and functionality

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 import './main.css'
+import data from './data/data.json';
 
 class Main extends React.Component {
   render() {
     return (
       <main>
-        {/* {data.map((hornObj, index)=> {
-
-        }
-        )} */}
-      <HornedBeast />
+        {data.map((hornObj, index)=> {
+          return <HornedBeast key={index} title={hornObj.title} image_url= {hornObj.image_url}
+          description={hornObj.description}/>
+        })}
+      
       </main>
     )
   }
