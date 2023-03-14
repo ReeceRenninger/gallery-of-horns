d8d979a54860ac358b0ee2981a28101b3892e0f3
ReeceRenninger <renningerreece@gmail.com>
2023-03-13 21:26:55 -0700

    stretch goal completed

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import HornData from './HornData';
 
 class HornedBeast extends React.Component{
   render(){
     return (
-      <>
-      <h2>{this.props.title}</h2>
-      <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
-      <p>{this.props.description}</p>
+      <section>
+        {HornData.map(animal=>(
+          <div key = {animal._id}>
+            <h2>{animal.title}</h2>
+            <img src={animal.image_url} alt={animal.description} title={animal.title} width="500px" height="500px"/>
+            <p>{animal.description}</p>
+          </div>
+        ))}
+        </section>
+      // <>
+      // <h2>{this.props.title}</h2>
+      // <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
+      // <p>{this.props.description}</p>
 
-      </>
-    )
+      // </>
+    );
   }
 }
 
--- src/Main.js
+++ src/Main.js
@@ -5,12 +5,13 @@ import HornedBeast from './HornedBeast';
 class Main extends React.Component {
   render() {
     return (
-      <>
-        <HornedBeast title="Dried Out Skeleton Horns" imageUrl="https://images.unsplash.com/photo-1523215887625-196ed74db8af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" description="dried out bone skeleton horns" />
+      <HornedBeast/>
+      // <>
+      //   <HornedBeast title="Dried Out Skeleton Horns" imageUrl="https://images.unsplash.com/photo-1523215887625-196ed74db8af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" description="dried out bone skeleton horns" />
 
-        <HornedBeast title="Decorative Bull Horns" imageUrl="https://images.unsplash.com/photo-1597333119576-acbc21a168fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" description="gold skull with designs and black horns" />
-        {/* <Hornedbeast title="" description="" imageurl=""/> */}
-      </>
+      //   <HornedBeast title="Decorative Bull Horns" imageUrl="https://images.unsplash.com/photo-1597333119576-acbc21a168fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" description="gold skull with designs and black horns" />
+      //   {/* <Hornedbeast title="" description="" imageurl=""/> */}
+      // </>
     )
   }
 }
