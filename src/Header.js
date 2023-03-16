d8dda204ca8c95b716624882b5128f625d6ca746
ReeceRenninger <renningerreece@gmail.com>
2023-03-16 15:34:08 -0700

    fixing header to have linear gradient

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 
 class Header extends React.Component{
   render(){
-    return <h1 class="w3-animate-fading">{this.props.unicorn}The Gallery of Fantastic Horns {this.props.rhino}</h1>
+    return <h1 >{this.props.unicorn}The Gallery of Fantastic Horns {this.props.rhino}</h1>
   }
 }
 
